79928d6c0ec5728efb783e9af7331ef729cf934d
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-06-12 14:46:03 +0200

    add products to homepage

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,9 +1,19 @@
+import ProductCard from "@components/products/ProductCard";
+import data from "@lib/data";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <h2 className="text-2xl py-2">Produits Récents</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductCard
+            key={product.slug}
+            product={product}
+          />
+        ))}
+      </div>
     </>
   );
 }
