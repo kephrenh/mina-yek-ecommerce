5f6728c3de886b28ecc12ab2ebc2389b1eaed3a6
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-06-28 15:32:24 +0200

    setup react-hot-toast

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,9 +1,14 @@
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@lib/auth";
+import ClientProviders from "./ClientProviders";
 // import ClientProviders from './ClientProviders'
 
 export default async function Providers({ children }: { children: React.ReactNode }) {
   const session = await auth();
 
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider session={session}>
+      <ClientProviders>{children}</ClientProviders>
+    </SessionProvider>
+  );
 }
